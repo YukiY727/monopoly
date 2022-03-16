7a8c02c9953669798c4ec2824df9f3b0bdf8655a
イシヒラ <83456982+Ishihira@users.noreply.github.com>
2022-03-16 12:00:24 +0900

    Land.auction()の機能を実装

--- main.py
+++ main.py
@@ -34,61 +34,102 @@ def query_yes_no(question, default="yes"):
         elif "no".startswith(choice):
             return False
         else:
-            sys.stdout.write("Please respond with 'yes' or 'no' " "(or 'y' or 'n').\n")
+            sys.stdout.write("Please respond with 'yes' or 'no' "
+                             "(or 'y' or 'n').\n")
+
 
 class Board:
+
     def __init__(self, members: list[Player]):
         self.members = members
         self.members_name = [member.name for member in members]
+        self.name_member_mapping = {
+            name: player
+            for name, player in zip(self.members_name, self.members)
+        }
+
 
 class Land:
+
     def __init__(self, name: str, price: int):
         self.name = name
         self.price = price
         self.is_own = False
         self.owner = None
 
-    def be_bought(self, plyer: Player ):
+    def be_bought(self, plyer: Player, board: Board):
         answer = query_yes_no(f'Would you buy this land for {self.price} ?')
         if answer:
             plyer.land.append()
             plyer.money -= self.price
         else:
-            self.auction()
+            self.auction(board)
+
     def auction(self, board: Board):
+        auction_price = None
         while True:
-            sys.stdout.write(f'{self.name} is now for sale. Please include your user name and  the amount you can offer')
+            sys.stdout.write(
+                f'{self.name} is now for sale. Please include your user name and the amount you can offer\n'
+            )
             username = input('Enter your name: ')
+            # TODO:usernameを間違えると'Please enter...が出続ける点の修正'
             while username not in board.members_name:
                 print('Please enter the correct username')
+            user = board.name_member_mapping[username]
+            offered_price = int(input("Enter a price you can offer: "))
+            if auction_price:
+                if offered_price > auction_price:
+                    auction_price = offered_price
+                    self.owner = user
+                else:
+                    sys.stdout.write(
+                        'offered price is not higher than the amount offered before\n'
+                    )
+            else:
+                auction_price = offered_price
+                self.owner = user
+            sys.stdout.write(
+                f'{self.name}\'s price is {auction_price} now (offered by {self.owner.name})\n'
+            )
+            sys.stdout.write(
+                f'Is there anyone who buys {self.name} at a higher price?\n')
+            continue_auction = yes_no_input()
+            if not continue_auction:
+                sys.stdout.write(
+                    f'{self.name} is bought by {self.owner.name} for {auction_price}!'
+                )
+                self.is_own = True
+                self.owner.money -= auction_price
+                break
 
 
 class Player:
+
     def __init__(self, name: str):
         self.name = name
-        self.land  = []
+        self.land = []
         self.money = 1500
 
 
-class Building:
-    def __init__(self):
+# class Building:
+#     def __init__(self):
+
+# class Train:
+#     def __init__(self):
+
+# class Waterworks:
+#     def __init__(self):
+
+# class Electric:
+#     def __init__(self):
+
+# class Chance:
+#     def __init__(self):
 
 
-class Train:
-    def __init__(self):
-
-class Waterworks:
-    def __init__(self):
-
-class Electric:
-    def __init__(self):
-
-class Chance:
-    def __init__(self):
-
-class Pool:
-    def __init__(self):
-#%%
+# class Pool:
+#     def __init__(self):
+# %%
 def yes_no_input():
     while True:
         choice = input("Please respond with 'yes' or 'no' [y/N]: ").lower()
@@ -96,7 +137,15 @@ def yes_no_input():
             return True
         elif choice in ['n', 'no']:
             return False
-yes_no_input()
+
+
 # %%
 query_yes_no('ok')
 # %%
+# test
+yusaku = Player('yusaku')
+takeshun = Player('takeshun')
+board = Board([yusaku, takeshun])
+land = Land('test', 200)
+#%%
+land.be_bought(yusaku, board)
